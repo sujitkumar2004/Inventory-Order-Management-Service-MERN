3378c36a2e8158364cf5385b9db1007eec5c49be
SUJIT KUMAR <127448852+sujitkumar2004@users.noreply.github.com>
2025-11-09 18:04:02 +0530

    Obfuscate contact details in Contact.jsx
    Updated contact information for privacy.

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>Natwar, Ambika Market, Near PNB Bank <br /> Bihar – 802218, India</p>
-          <p className=' text-gray-500'>Tel: +91 8873833600 <br /> Email: krishnavastralaya@gmail.com</p>
+          <p className=' text-gray-500'>Natxxx, Ambika Market, Near PNB Bank <br /> Bihar – 8022xx, India</p>
+          <p className=' text-gray-500'>Tel: +91 8873xx3600 <br /> Email: krixxxxvastralaya@gmail.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
